7b20dfeaa2970dde0b0114dbc7da2bb6f922dc68
Quintyn Williams <t.quin.williams@gmail.com>
2023-05-08 11:12:05 -0500

    welcome servlet form

--- src/main/java/WelcomeServlet.java
+++ src/main/java/WelcomeServlet.java
@@ -12,13 +12,31 @@ import java.io.PrintWriter;
 public class WelcomeServlet extends HttpServlet {
 
     @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String car = req.getParameter("car");
+        resp.setContentType("text/html");
+        PrintWriter out =resp.getWriter();
+
+        if (car != null) {
+            out.println("<h3>Your&nbsp" + car + " is awesome!<h3>");
+        } else {
+            out.print("<h3>Tell me about your car!<h3>");
+        }
+
+//  TEST: TYPE http://localhost:8080/welcome?car=[something] in the URL path
+    }
+
+    @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
 //  HttpServletRequest getting what is sent saved as data-type String
         String username = req.getParameter("username");
+
         resp.setContentType("text/html");
         PrintWriter out = resp.getWriter();
-        out.println("<h2>Welcome, " + username + "!</h2>");
+
+        out.println("<h2>Welcome,&nbsp" + username + "!</h2>");
 
     }
 
